bd1bdb23c8b3c6f86036a502197679d8e32d993e
Jasbindar Singh <jasbindar1998@gmail.com>
2021-06-20 23:46:49 +0530

    [Style] Design tokens added

---
+++ src/token/token.js
@@ -0,0 +1,48 @@
+const colors = {
+  primary: {
+    default: '#26d07c',
+    dark: '#465062' 
+  },
+  typography: {
+    light: {
+      default: 'rgba(255, 255, 255, 0.8)'
+    },
+    dark: {
+      default: 'rgba(0, 0, 0, 0.8)'
+    }
+  }
+}
+
+const typography = {
+  size: {
+    16: 16,
+    21: 21,
+    24: 24,
+    36: 36,
+    54: 54,
+    81: 81
+  },
+  font: {
+    work: ['Work Sans', 'sans-serif'],
+    epilogue: ['Epilogue', 'sans-serif']
+  }
+}
+
+const spacing = {
+  4: 4,
+  8: 8,
+  16: 16,
+  24: 24,
+  32: 32,
+  40: 40,
+  48: 48,
+  56: 56,
+  72: 72,
+  80: 80,
+}
+
+module.exports = {
+  colors,
+  typography,
+  spacing
+}
