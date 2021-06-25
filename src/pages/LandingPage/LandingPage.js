735716e9a811a10a458913b4a24fb7f287f91ede
Piyush Sinha <piyush.sinha24@gmail.com>
2021-06-25 13:29:36 +0530

    Fix: Mobile layout added

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -4,23 +4,31 @@ import bottomSquares from '../../assets/images/bottom-squares.svg';
 
 function LandingPage() {
   return (
-    <div className="w-screen h-screen flex flex-col items-start">
-      <img className="w-1/6" src={topSquares} alt="top-squares"></img>
-      <div className="flex">
-        <div className="font-bold text-81 text-typography-dark-default font-epilogue mx-80 my-32">
-          ConnectEm
+    <div className="w-screen h-screen flex flex-col items-start justify-between">
+      <img className="w-1/3 md:w-1/6" src={topSquares} alt="top-squares"></img>
+      <div>
+        <div className="flex">
+          <div className="font-bold text-54 md:text-81 text-typography-dark-default font-epilogue mx-40 md:mx-80">
+            ConnectEm
+          </div>
+          <img
+            className="hidden md:block md:w-2/5 lg:w-full"
+            src={waves}
+            alt="waves"
+          ></img>
         </div>
-        <img src={waves} alt="waves"></img>
+        <div className="mx-48 md:mx-88">
+          <div className="text-24 text-primary-dark font-work">Your one stop</div>
+          <div className="text-24 text-primary-dark font-work">
+            for scheduling meeting
+          </div>
+        </div>
+        <button className="rounded font-bold text-16 text-typography-light-default bg-primary-default shadow-xl hover:bg-primary-dark mx-48 md:mx-88 my-16 p-16">
+          Get Started
+        </button>
       </div>
-      <div className="text-24 text-primary-dark font-work mx-80">Your one stop</div>
-      <div className="text-24 text-primary-dark font-work mx-80">
-        for scheduling meeting
-      </div>
-      <button className="rounded font-bold text-16 text-typography-light-default bg-primary-default hover:shadow-xl hover:bg-primary-dark mx-80 my-16 p-16">
-        Get Started
-      </button>
       <img
-        className="w-2/5 absolute bottom-0 right-0"
+        className="w-2/3 md:w-2/5 self-end"
         src={bottomSquares}
         alt="top-squares"
       ></img>
