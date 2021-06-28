97053f3dc53dd677d67f5865aa20ca226ae265aa
Piyush Sinha <piyush.sinha24@gmail.com>
2021-06-28 18:21:58 +0530

    [Build] Dashboard added

---
+++ src/services/eventService.js
@@ -0,0 +1,19 @@
+import protectedAxios from './axios/protectedAxios';
+
+export const userEvents = async () => {
+  try {
+    const { data } = await protectedAxios.get(`/event`);
+    return { data };
+  } catch (error) {
+    return { error };
+  }
+};
+
+export const toggleEvent = async (params) => {
+  try {
+    const { data } = await protectedAxios.post(`/event/toggle/${params}`);
+    return { data };
+  } catch (error) {
+    return { error };
+  }
+};
